f009b3531f513d8dceb088acbdeb10156afe2219
Abdirahman Abdullahi <167759102+Abdiggity@users.noreply.github.com>
2025-11-10 11:49:39 +0000

    Update R movement physiolgy results.R

--- R movement physiolgy results.R
+++ R movement physiolgy results.R
@@ -1,35 +1,48 @@
-setwd("C:/Users/612ra/OneDrive/Documents/UNI/movement physilogy")
+## Movement physiology – repeated-measures ANOVA on jump height
 
+# install.packages("ez")      # run once
+# install.packages("ggplot2") # run once
 
-install.packages('ez')
-library(ez) 
-
-mydataset = read.csv('jumpheightANOVA.csv')
-attach(mydataset)
-names(mydataset)
-data
-
-myresults = ezANOVA(data=mydataset,dv=.(Height),wid=.(ID),within=.(Type),type=3)
-myresults
-mydataset$Type<- factor(mydataset$Type, levels = c("S", "CMJ", "CMJA"))
-dev.off()
-boxplot(mydataset$Height~mydataset$Type, ylab = "HEIGHT IN CM", xlab = "TYPE OF JUMP",col= c("green","yellow","red"))
-install.packages("ggpot2")
+library(ez)
 library(ggplot2)
-ggplot(mydataset, aes(x=Type, y=Height ,fill=Type)) + geom_boxplot( )+ theme_classic()+scale_fill_manual(values=c("green", "orange","red"),ylab = "HEIGHT IN CM", xlab = "TYPE OF JUMP")
-      
-        
-        
-       
-        
-#For extra credit in your lab case study, consider evidencing the following things:
-# Check the results for the 'Mauchly Test for Sphericity'. Do some reading around the test. What does this mean when the test p < 0.05.
-#   Should you use the uncorrected results, or the Sphericity Corrected Results?
 
+## 1. Load data ---------------------------------------------------------
+# better than setwd() for GitHub/shared code
+data <- read.csv("jumpheightANOVA.csv")
 
-# The default boxplot is quite ugly. Spend time making it more intuitive to readers:
-#   Consider rearranging the order of the boxes into something more intuitive e.g. S, CMJ, CMJA 
-#   Hint: https://www.r-graph-gallery.com/9-ordered-boxplot.html
-# Improve the axis labels and make sure you include units
-# Replace the default grey fill of the boxes with something else
-# Get rid of the black box frame surrounding the plot
+# make sure factors are set
+data$ID   <- as.factor(data$ID)
+data$Type <- factor(data$Type, levels = c("S", "CMJ", "CMJA"))  # ordered
+
+## 2. Repeated-measures ANOVA ------------------------------------------
+res <- ezANOVA(
+  data   = data,
+  dv     = .(Height),
+  wid    = .(ID),
+  within = .(Type),
+  type   = 3,
+  detailed = TRUE
+)
+print(res)
+
+# note for your write-up:
+# if Mauchly’s test (in res$`Mauchly's Test for Sphericity`) is p < 0.05,
+# report the sphericity-corrected (Greenhouse-Geisser) results.
+
+## 3. Base R boxplot ----------------------------------------------------
+boxplot(
+  Height ~ Type,
+  data = data,
+  ylab = "Height (cm)",
+  xlab = "Type of jump",
+  col  = c("green", "yellow", "red"),
+  frame.plot = FALSE
+)
+
+## 4. ggplot version ----------------------------------------------------
+ggplot(data, aes(x = Type, y = Height, fill = Type)) +
+  geom_boxplot() +
+  scale_fill_manual(values = c("green", "orange", "red")) +
+  labs(x = "Type of jump", y = "Height (cm)") +
+  theme_classic() +
+  theme(legend.position = "none")
